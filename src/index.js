42cb0f7042953a0a2c2b9345b9231a67a363a5e0
Ben Cullen <benjaminjcullen1@gmail.com>
2022-06-12 09:23:42 +0100

    first component created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// Create a React Component
+const App = () => {
+    return <div>Hi there!</div>;
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Take the React Component and display it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
